97baa4711b147889ec571f5d853c4460c2dc6d44
edEssential <ed@essential-music.com>
2014-03-05 11:40:56 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+V1::Application.config.secret_token = '7915a028d70d7b1e9c2f3dace9f8b295af1f679f16cc8468cda513ed596c901f6c9cd5a06171def5676037f51b71d53f3766d621543b6537e01e06cad56ae385'
