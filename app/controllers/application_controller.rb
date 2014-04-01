0ca4a141bc5eab3551c9e35d9fcc3862307f11ba
edEssential <ed@essential-music.com>
2014-04-01 11:44:05 +0100

    title slides now displaying with ID 1 slide first

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   def bring_in_models
-    @heros = Hero.order("id DESC")
+    @heros = Hero.order("id ASC")
     @abouttb1 = Abouttb.first
     @abouttbs = Abouttb.all[1..-1]
     @abouttbsall = Abouttb.all
