d02ef3fe91164daa6be939d5581085dd7d199613
madebyeggs <madebyeggs@gmail.com>
2016-11-02 18:45:18 +0000

    paperclip method removed from heros controller

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,11 +1,3 @@
 class Hero < ActiveRecord::Base
   
-  has_attached_file :image, styles: {
-      thumb: '100x100>',
-      square: '200x200#',
-      medium: '1920x1080>'
-  }
-  
-  # Validate the attached image is image/jpg, image/png, etc
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
